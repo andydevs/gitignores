c161951d0ef4aef6ea0b890d5734c87e113043e6
andydev <akanshul97@gmail.com>
2017-06-17 00:07:05 -0400

    Created spec skeleton

--- spec/gitignores-spec.js
+++ spec/gitignores-spec.js
@@ -1,73 +1,37 @@
 'use babel';
+/**
+ * Gitignores
+ *
+ * Creates gitignore files
+ *
+ * @author  Andydevs
+ * @created 6 - 16 - 2017
+ */
 
-import Gitignores from '../lib/gitignores';
+// Imports
+import Gitignores from '../lib/gitignores'
 
-// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
-//
-// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
-// or `fdescribe`). Remove the `f` to unfocus the block.
-
+// Describe Gitignores
 describe('Gitignores', () => {
-  let workspaceElement, activationPromise;
+    // Describe create command
+    describe('gitignores:create', () => {
+        it('Shows the selector view', () => {})
+        it('Shows the selector view panel', () => {})
+    })
 
-  beforeEach(() => {
-    workspaceElement = atom.views.getView(atom.workspace);
-    activationPromise = atom.packages.activatePackage('gitignores');
-  });
+    // When selector view is open
+    describe('When selector view is open', () => {
+        // Describe cancel command
+        describe('core:cancel', () => {
+            it('Closes the selector view', () => {})
+            it('Closes the selector view panel', () => {})
+        })
 
-  describe('when the gitignores:toggle event is triggered', () => {
-    it('hides and shows the modal panel', () => {
-      // Before the activation event the view is not on the DOM, and no panel
-      // has been created
-      expect(workspaceElement.querySelector('.gitignores')).not.toExist();
-
-      // This is an activation event, triggering it will cause the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'gitignores:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        expect(workspaceElement.querySelector('.gitignores')).toExist();
-
-        let gitignoresElement = workspaceElement.querySelector('.gitignores');
-        expect(gitignoresElement).toExist();
-
-        let gitignoresPanel = atom.workspace.panelForItem(gitignoresElement);
-        expect(gitignoresPanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, 'gitignores:toggle');
-        expect(gitignoresPanel.isVisible()).toBe(false);
-      });
-    });
-
-    it('hides and shows the view', () => {
-      // This test shows you an integration test testing at the view level.
-
-      // Attaching the workspaceElement to the DOM is required to allow the
-      // `toBeVisible()` matchers to work. Anything testing visibility or focus
-      // requires that the workspaceElement is on the DOM. Tests that attach the
-      // workspaceElement to the DOM are generally slower than those off DOM.
-      jasmine.attachToDOM(workspaceElement);
-
-      expect(workspaceElement.querySelector('.gitignores')).not.toExist();
-
-      // This is an activation event, triggering it causes the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'gitignores:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        // Now we can test for view visibility
-        let gitignoresElement = workspaceElement.querySelector('.gitignores');
-        expect(gitignoresElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, 'gitignores:toggle');
-        expect(gitignoresElement).not.toBeVisible();
-      });
-    });
-  });
-});
+        // Describe confirm command
+        describe('core:confirm', () => {
+            it('Closes the selector view', () => {})
+            it('Closes the selector view panel', () => {})
+            it('Creates a new .gitignore', () => {})
+        })
+    })
+})
