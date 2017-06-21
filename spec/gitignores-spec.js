91e9d93a179b60a5c5b8fed51ed6cf5d506a77c1
andydev <akanshul97@gmail.com>
2017-06-20 22:13:50 -0400

    Implemented first test

--- spec/gitignores-spec.js
+++ spec/gitignores-spec.js
@@ -10,27 +10,47 @@
 
 // Imports
 import Gitignores from '../lib/gitignores'
+import SelectListView from 'atom-select-list'
 
 // Describe Gitignores
 describe('Gitignores', () => {
+    // Variables
+    var activatedPromise
+    var workspaceElement
+
+    // Do before each
+    beforeEach(() => {
+        activatedPromise = atom.packages.activatePackage('gitignores')
+        workspaceElement = atom.views.getView(atom.workspace)
+        jasmine.attachToDOM(workspaceElement)
+    })
+
     // Describe create command
     describe('gitignores:create', () => {
-        it('Shows the selector view', () => {})
-        it('Shows the selector view panel', () => {})
+        it('Shows the selector panel', () => {
+            // Dispatch command
+            atom.commands.dispatch(workspaceElement, 'gitignores:create')
+            waitsForPromise(() => activatedPromise)
+
+            // Run tests
+            var panels = atom.workspace.getModalPanels()
+            expect(panels.length).toBeGreaterThan(0)
+            var panel = panels[0]
+            expect(panel.getItem().classList.contains('select-list')).toBeTruthy()
+            expect(panel.getItem().classList.contains('gitignores')).toBeTruthy()
+        })
     })
 
     // When selector view is open
     describe('When selector view is open', () => {
         // Describe cancel command
         describe('core:cancel', () => {
-            it('Closes the selector view', () => {})
-            it('Closes the selector view panel', () => {})
+            it('Closes the selector panel', () => {})
         })
 
         // Describe confirm command
         describe('core:confirm', () => {
-            it('Closes the selector view', () => {})
-            it('Closes the selector view panel', () => {})
+            it('Closes the selector panel', () => {})
             it('Creates a new .gitignore', () => {})
         })
     })
